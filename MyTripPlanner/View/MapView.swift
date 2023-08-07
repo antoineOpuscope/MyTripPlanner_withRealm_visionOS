b91117d98fd1a926b53625d7a22069b0dd4789bc
Antoine OMNES <antoine@minsar.com>
2023-08-07 17:40:25 +0200

    first implementation of MapView

--- MyTripPlanner/View/MapView.swift
+++ MyTripPlanner/View/MapView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 48.858046588769085, longitude:  2.2949914738436776),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region)
+            .ignoresSafeArea()
     }
 }
 
