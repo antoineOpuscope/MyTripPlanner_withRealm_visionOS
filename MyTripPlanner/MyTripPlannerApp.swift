a4960a92e95870061caebbcb86876af699ca46c5
Antoine OMNES <antoine@minsar.com>
2023-08-07 16:59:56 +0200

    init base project

---
+++ MyTripPlanner/MyTripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTripPlannerApp.swift
+//  MyTripPlanner
+//
+//  Created by Antoine Omnès on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyTripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
