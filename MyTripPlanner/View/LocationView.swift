27037e048e33aced996de6a99886b77aa42fca2d
Antoine OMNES <antoine@minsar.com>
2023-08-08 17:56:21 +0200

    LocationView add content (not yet working perfectly)

--- MyTripPlanner/View/LocationView.swift
+++ MyTripPlanner/View/LocationView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import SymbolPicker
+import EmojiPicker
 
 struct LocationView: View {
     var body: some View {
         
         @State var isEditing: Bool = true
+        @State var isEmojiPickerPresented: Bool = false
+        @State var isSymbolPickerPresented: Bool = false
+        
+        @State var isReserved: Bool = false
+        @State var isFavorite: Bool = false
+        @State var price: Float = 0
+        @State var icon = "mappin"
         
         NavigationStack {
             VStack {
@@ -27,36 +36,81 @@ struct LocationView: View {
                                     .frame(width: 30, height: 30)
                                     .foregroundColor(.cyan)
                                 
-                                Image(systemName: "mappin")
+                                Image(systemName: icon)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 20, height: 20)
                             }
                             Spacer()
-                            Button("Choose color") {}
-                                .padding(5)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(.blue, lineWidth: 1)
+                            if (isEditing) {
+                                Button("Choose color") {
+                                    
                                 }
-                            Button("Choose icon")  {}
-                                .padding(5)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(.blue, lineWidth: 1)
+                                    .padding(5)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(.blue, lineWidth: 1)
+                                    }
+                                Button("Choose icon")  {
+                                    isSymbolPickerPresented = true
                                 }
-                            
+                                    .padding(5)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(.blue, lineWidth: 1)
+                                    }
+                            }
                         }
                         
                     } header: {
                         Text("Visual")
                     }
+                    
+                    Section {
+                        Text("Dasdad")
+                    } header: {
+                        Text("Description")
+                    }
+                    
+                    Section {
+                        Toggle(isOn: $isReserved) {
+                            Text("Booked :")
+                        }
+                        .toggleStyle(.switch)
+                        .disabled(isEditing == false)
+                        
+                        Text("Price: \(price.formatted()) $")
+                        
+                        
+                    } header: {
+                        Text("Booking info")
+                    }
+                    
+                    Section {
+                        Button("Open with GPS") {}
+                    } header: {
+                        Text("Actions")
+                    }
                 }
                 
             }.navigationTitle("Pin")
                 .toolbar {
-                    Button(isEditing ? "Save" : "Edit") {}
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(isEditing ? "Save" : "Edit") { isEditing.toggle()
+                        }
+                    }
+                    ToolbarItem(placement: .bottomBar) {
+                        Button {
+                                isFavorite.toggle()
+                        } label: {
+                            Image(systemName: isFavorite ? "bookmark.fill" : "bookmark")
+                        }
+                    }
                 }
+        }.sheet(isPresented: $isEmojiPickerPresented) {
+            SymbolPicker(symbol: $icon)
+        }.sheet(isPresented: $isSymbolPickerPresented) {
+            SymbolPicker(symbol: $icon)
         }
         
     }
