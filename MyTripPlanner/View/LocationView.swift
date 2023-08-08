980fb337aae48e1981236f77b0e62684337e79c6
Antoine OMNES <antoine@minsar.com>
2023-08-08 16:53:27 +0200

    init ui LocationView

--- MyTripPlanner/View/LocationView.swift
+++ MyTripPlanner/View/LocationView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct LocationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        @State var isEditing: Bool = true
+        
+        NavigationStack {
+            VStack {
+                MapView()
+                    .allowsHitTesting(false)
+                    .frame(height: 300)
+                    .padding(.top)
+                
+                Form {
+                    Section {
+                        HStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.cyan)
+                                
+                                Image(systemName: "mappin")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 20, height: 20)
+                            }
+                            Spacer()
+                            Button("Choose color") {}
+                                .padding(5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.blue, lineWidth: 1)
+                                }
+                            Button("Choose icon")  {}
+                                .padding(5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.blue, lineWidth: 1)
+                                }
+                            
+                        }
+                        
+                    } header: {
+                        Text("Visual")
+                    }
+                }
+                
+            }.navigationTitle("Pin")
+                .toolbar {
+                    Button(isEditing ? "Save" : "Edit") {}
+                }
+        }
+        
     }
 }
 
