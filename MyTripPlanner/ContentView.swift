ece1a8947d9c8a4cc34eb41d0a72793dfeff74e7
Antoine OMNES <antoine@minsar.com>
2023-08-08 17:55:46 +0200

    fill fake ContentView

--- MyTripPlanner/ContentView.swift
+++ MyTripPlanner/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var user: User = User()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(0...11, id: \.self) {i in
+                    NavigationLink {
+                        ProjectView()
+                    } label: {
+                        HStack {
+                            Text("Project \(i)")
+                        }
+                        
+                    }
+                }
+            }.navigationTitle("Projects")
+                .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
