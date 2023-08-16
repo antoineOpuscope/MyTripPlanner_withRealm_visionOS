3a4727c0ded9e0d2644fa524b9f66fb69c55be6d
Antoine OMNES <antoine@minsar.com>
2023-08-16 11:16:32 +0200

    add enviroment object on sheet

--- MyTripPlanner/ContentView.swift
+++ MyTripPlanner/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
                 }
                 .sheet(isPresented: $isCreateProjectViewPresented) {
                     ProjectCreationView()
-                }
+                }.environmentObject(stateController)
         }
     }
 }
