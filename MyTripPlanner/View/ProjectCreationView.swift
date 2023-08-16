36dbcfdb3ba168a4321d86b1c2236df2cffe830a
Antoine OMNES <antoine@minsar.com>
2023-08-16 10:41:27 +0200

    init creation project view

---
+++ MyTripPlanner/View/ProjectCreationView.swift
@@ -0,0 +1,57 @@
+//
+//  ProjectCreationView.swift
+//  MyTripPlanner
+//
+//  Created by Antoine Omnès on 16/08/2023.
+//
+
+import SwiftUI
+
+struct ProjectCreationView: View {
+    
+    @State private var name: String = ""
+    @State private var description: String = ""
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Project creation")
+                    .font(.title)
+                HStack {
+                    Text("Name : ")
+                    TextField("Enter project name...", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                }
+                HStack {
+                    Text("Description : ")
+                    TextField("Enter description...", text: $description, axis: .vertical)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Spacer()
+            }.padding()
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            Text("Save")
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            self.dismiss()
+                        } label: {
+                            Text("Cancel")
+                                .foregroundStyle(.red)
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ProjectCreationView()
+}
