85b666e56104a486b417dc897b22671f81cd30c6
Antoine OMNES <antoine@minsar.com>
2023-08-25 14:51:35 +0200

    init ui merge projects

--- MyTripPlanner/View/AllProjectsView.swift
+++ MyTripPlanner/View/AllProjectsView.swift
@@ -13,9 +13,14 @@ struct AllProjectsView: View {
     
     @State var isCreateProjectViewPresented: Bool = false
     
+    @State private var multiSelection = Set<UUID>()
+    @State private var isPresentedMergeConfirmationAlert = false
+    
+    @State var isMergingProjects = false
+    
     var body: some View {
         NavigationStack {
-            List {
+            List(selection: $multiSelection) {
                 ForEach(stateController.projects, id: \.id) {project in
                     NavigationLink {
                         ProjectView(project: project)
@@ -28,16 +33,62 @@ struct AllProjectsView: View {
                 }
             }.navigationTitle("Projects")
                 .navigationBarTitleDisplayMode(.inline)
+                .environment(\.editMode, .constant(self.isMergingProjects ? EditMode.active : EditMode.inactive))
                 .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
+                    ToolbarItem(placement: .topBarTrailing) {
                         Button("Add", systemImage: "plus") {
                             self.isCreateProjectViewPresented = true
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        if isMergingProjects {
+                            
+                            Button {
+                                self.isMergingProjects.toggle()
+                            } label: {
+                                Image(systemName: "multiply")
+                                    .foregroundStyle(.red)
+                            }
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        if isMergingProjects == false {
+                            Button("Add", systemImage: "arrow.triangle.merge") {
+                                self.isMergingProjects.toggle()
+                            }
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        if isMergingProjects && self.multiSelection.count >= 2 {
+                            Button("arrow.triangle.merge", systemImage: "checkmark") {
+                                
+                                //
+                                self.isPresentedMergeConfirmationAlert = true
+                            }
                         }
                     }
                 }
                 .sheet(isPresented: $isCreateProjectViewPresented) {
                     ProjectCreationView()
                         .environmentObject(stateController)
+                }
+                .alert(isPresented: $isPresentedMergeConfirmationAlert) {
+                    
+                    Alert(title: Text("Merging"),
+                          message: Text("Are you sur you want to merge the selected projects ?"),
+                          primaryButton: .destructive(
+                            Text("Cancel"),
+                            action: {
+                                
+                            }
+                          ),
+                          secondaryButton: .default(
+                            Text("Confirm"),
+                            action: {
+                                
+                            }
+                        )
+                    )
                 }
         }
     }
